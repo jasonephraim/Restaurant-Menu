56bf40dc5ba1053d517c9ce9a859ecbbd0e98884
Jason Ephraim <jason.ephraim.novatex@gmail.com>
2020-04-26 11:45:45 -0500

    added array filters

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
-let renderDishesList = document.getElementById("dishes");
+var renderDishesList = document.getElementById("dishes");
+const dishesArray = Array.from(dishes);
 
-let dishesList = dishes.map(function (dish) {
+let dishesList = dishesArray.map(function (dish) {
   let dishesInList = `
     <div class="dish-card card col-lg-3 col-md-4 col-sm-12 m-4 ${dish.course}">
       <img class="card-img-top" src="${dish.imageURL}" alt="Card image cap">
@@ -9,11 +10,20 @@ let dishesList = dishes.map(function (dish) {
         <p class="card-text">${dish.description}</p>
       </div>
       <ul class="list-group list-group-flush">
-        <li class="list-group-item">Course: ${dish.course}</li>
-        <li class="list-group-item">Price: $${dish.price}</li>
+        <li class="list-group-item"><span class="bold">Course:</span> ${dish.course}</li>
+        <li class="list-group-item"><span class="bold">Price:</span> $${dish.price}</li>
       </ul>
     </div>`;
   return dishesInList;
 });
 
 renderDishesList.innerHTML = dishesList.join(" ");
+
+var startersArray = dishesArray.filter((dish) => dish.course === "Starters");
+console.log(startersArray);
+
+var entreesArray = dishesArray.filter((dish) => dish.course === "Entrees");
+console.log(entreesArray);
+
+var dessertsArray = dishesArray.filter((dish) => dish.course === "Desserts");
+console.log(dessertsArray);
