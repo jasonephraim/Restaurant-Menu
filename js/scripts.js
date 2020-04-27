2d3993796673d01d6a15fdaf8708f9e2ece0f00d
Jason Ephraim <jason.ephraim.novatex@gmail.com>
2020-04-26 19:31:43 -0500

    Added working content filter

--- js/scripts.js
+++ js/scripts.js
@@ -19,11 +19,48 @@ let dishesList = dishesArray.map(function (dish) {
 
 renderDishesList.innerHTML = dishesList.join(" ");
 
-var startersArray = dishesArray.filter((dish) => dish.course === "Starters");
-console.log(startersArray);
+// var startersArray = dishesArray.filter((dish) => dish.course === "Starters");
+// console.log(startersArray);
 
-var entreesArray = dishesArray.filter((dish) => dish.course === "Entrees");
-console.log(entreesArray);
+//var entreesArray = dishesArray.filter((dish) => dish.course === "Entrees");
+//console.log(entreesArray);
 
-var dessertsArray = dishesArray.filter((dish) => dish.course === "Desserts");
-console.log(dessertsArray);
+//var dessertsArray = dishesArray.filter((dish) => dish.course === "Desserts");
+//console.log(dessertsArray);
+
+filterSelection("all");
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("dish-card");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
